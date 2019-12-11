da7074c2fe211a573227645b1d64ead9b75dd3a1
dheeraj <djshres@gmail.com>
2019-12-11 16:56:02 +0545

    Initial

---
+++ BlogAPI/Data/BlogPostsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlogAPI.Models;
+
+namespace BlogAPI.Data
+{
+    public class BlogPostsContext : DbContext
+    {
+        public BlogPostsContext (DbContextOptions<BlogPostsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPost { get; set; }
+    }
+}
